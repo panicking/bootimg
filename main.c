3f5c5aeb57e68f1038a5c31601cce818aa747caf
Meena <meena.murthy@amarulasolutions.com>
2022-04-22 16:55:05 +0530

    Add a function to compare the actual hash of boot image and info hash

--- main.c
+++ main.c
@@ -23,53 +23,89 @@
 #include <errno.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <stdbool.h>
 
 #include "bootimg.h"
 #include "mincrypt/sha.h"
+
+#define HASH_LENGTH (SHA_DIGEST_SIZE * 2 + 1)
 
 static char *read_hash(const byte *hash)
 {
 	char *str = malloc(SHA_DIGEST_SIZE * 2 + 1);
 	char *c = str;
 	const byte *h = hash;
-	for (; c < str + SHA_DIGEST_SIZE * 2; h++) {
+	for (; c < str + SHA_DIGEST_SIZE * 2; h++)
+	{
 		c += sprintf(c, "%02x", *h);
 	}
 	*c = 0;
 	return str;
 }
 
-void print_boot_info(const boot_img *image)
+bool compare_hashes(char *info_hash, char *info_actual_hash)
 {
-	char *hash = read_hash((byte*)image->hdr.hash);
-	if (hash) {
-		printf("IMAGE HASH 0x%s\n", hash);
-		free(hash);
-	}
 
-	byte *bytes = bootimg_generate_hash(image);
-	if (bytes) {
-		char *hash = read_hash(bytes);
-		if (hash) {
-			printf("CALCULATE HASH 0x%s\n", hash);
-			free(hash);
-		}
-		free(bytes);
-	}
+	if (!info_hash || !info_actual_hash)
+		return false;
+
+	if (strncmp(info_hash, info_actual_hash, HASH_LENGTH) == 0)
+		return true;
+
+	return false;
 }
 
-int main(const int argc, const char** argv)
+void print_boot_info(const boot_img *image)
+{
+	bool equal_hashes = false;
+	byte *bytes = NULL;
+	char *hash2 = NULL;
+
+	char *hash = read_hash((byte *)image->hdr.hash);
+
+	bytes = bootimg_generate_hash(image);
+	if (bytes)
+	{
+		hash2 = read_hash(bytes);
+	}
+
+	if (!hash2 || !hash)
+	{
+		printf("Unable to extract hashes from image\n");
+		return;
+	}
+
+	equal_hashes = compare_hashes(hash, hash2);
+
+	if (equal_hashes)
+	{
+		printf("Both the HASHES are equal to %s\n", hash);
+	}
+	else
+	{
+		printf("HASHES are different:\nIMAGE_HASH 0x%s\nCALCULATE_HASH 0x%s\n",
+			   hash, hash2);
+	}
+
+	free(hash);
+	free(hash2);
+	free(bytes);
+}
+
+int main(const int argc, const char **argv)
 {
 	int ret = 0;
 	boot_img *image = 0;
 
-	if (argc <= 1) {
+	if (argc <= 1)
+	{
 		printf("Not enough arguments!\nUse imageinfo <android boot image file>\n");
 		return 0;
 	}
 
 	image = load_boot_image(argv[1]);
-	if (!image) {
+	if (!image)
+	{
 		printf("Fail to load boot image\n");
 		return 0;
 	}
