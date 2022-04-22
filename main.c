707107d5b8ca9a21a7b7e5f8dd856f4202785ac7
Meena <meena.murthy@amarulasolutions.com>
2022-04-22 16:55:05 +0530

    Add a function to compare the actaul hash and generate one
    This function compare the calculate hash, with the hash stored
    in image header. This can be then extract in a library to verify
    rockchip image integrity
    
    Signed-off-by: Michael Trimarchi <michael@amarulasolutions.com>

--- main.c
+++ main.c
@@ -23,9 +23,12 @@
 #include <errno.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <stdbool.h>
 
 #include "bootimg.h"
 #include "mincrypt/sha.h"
+
+#define HASH_SIZE (SHA_DIGEST_SIZE * 2 + 1)
 
 static char *read_hash(const byte *hash)
 {
@@ -39,23 +42,45 @@ static char *read_hash(const byte *hash)
 	return str;
 }
 
+bool compare_hashes(char* info_hash, char* info_actual_hash)
+{
+
+	if (!info_hash || !info_actual_hash)
+		return false;
+
+	if (strncmp(info_hash, info_actual_hash, HASH_SIZE) == 0)
+		return true;
+
+	return false;
+}
+
 void print_boot_info(const boot_img *image)
 {
+	bool equal_hashes = false;
+	byte *bytes = NULL;
+	char *hash2 = NULL;
+
 	char *hash = read_hash((byte*)image->hdr.hash);
-	if (hash) {
-		printf("IMAGE HASH 0x%s\n", hash);
-		free(hash);
+
+	bytes = bootimg_generate_hash(image);
+	if (bytes)
+		hash2 = read_hash(bytes);
+
+	if (!hash2 || !hash) {
+		printf("Unable to extract hashes from image\n");
+		return;
 	}
 
-	byte *bytes = bootimg_generate_hash(image);
-	if (bytes) {
-		char *hash = read_hash(bytes);
-		if (hash) {
-			printf("CALCULATE HASH 0x%s\n", hash);
-			free(hash);
-		}
-		free(bytes);
-	}
+	equal_hashes = compare_hashes(hash, hash2);
+	if (equal_hashes)
+		printf("HASH are equals to %s\n", hash);
+	else
+		printf("HASHES are different:\nIMAGE_HASH 0x%s\nCALCULATE_HASH 0x%s\n",
+			hash, hash2);
+
+	free(hash);
+	free(hash2);
+	free(bytes);
 }
 
 int main(const int argc, const char** argv)
